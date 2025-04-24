2b1abd11123517546abef2412057a8bc2ea02244
Suman Kumar <suman.k9709@gmail.com>
2025-04-24 15:22:19 +0530

    react file

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+function App() {
+
+  const [task , setTask] = useState("");
+  const [list , setList] = useState([]);
+
+  return (
+    <>
+      <input type="text" value="abhi" />
+    </>
+  )
+}
+
+export default App
